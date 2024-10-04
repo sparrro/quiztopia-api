b56afdf80406a1ba2a86ea904421e7c586039902
sparrro <116090214+sparrro@users.noreply.github.com>
2024-10-04 02:38:22 +0200

    delete quiz done

--- functions/deleteQuiz/index.js
+++ functions/deleteQuiz/index.js
@@ -1,8 +1,33 @@
 
 const { db } = require("../../database/index");
-const {} = require("@aws-sdk/lib-dynamodb");
+const { DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 const { sendResponse, sendError } = require("../../responses/index");
+const middy = require("@middy/core");
+const { authenticate } = require("../../middy/index");
 
-exports.handler = async (event) => {
-    
+deleteQuizHandler = async (event) => {
+
+    //ta in data
+    const id = event.queryStringParameters["id"];
+
+    //kör
+    try {
+
+        const deleteCommand = new DeleteCommand({
+            TableName: "quizzes",
+            Key: { quizId: id },
+        });
+
+        await db.send(deleteCommand);
+
+        return sendResponse(200, "Quiz deleted");
+
+    } catch (error) {
+        console.error(error);
+        return sendError(500, "Server error");
+    }
+
+
 }
+
+exports.handler = middy(deleteQuizHandler).use(authenticate);
