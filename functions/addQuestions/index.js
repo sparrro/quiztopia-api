ff9ea0a15e1ad9d99d7d5b97c656fcf9f54595c0
sparrro <116090214+sparrro@users.noreply.github.com>
2024-10-04 02:05:41 +0200

    add questions done

--- functions/addQuestions/index.js
+++ functions/addQuestions/index.js
@@ -1,11 +1,38 @@
 const { db } = require("../../database/index");
-const {} = require("@aws-sdk/lib-dynamodb");
+const { UpdateCommand } = require("@aws-sdk/lib-dynamodb");
 const { sendResponse, sendError } = require("../../responses/index");
 const middy = require("@middy/core");
-const { authenticate } = require("../../middy");
+const { authenticate, validateQuestions } = require("../../middy");
 
 const addQuestionsHandler = async (event) => {
 
+    //ta in data
+    const id = event.queryStringParameters["id"];
+    const { quizQuestions } = JSON.parse(event.body);
+
+    //kör
+    try {
+
+        const updateCommand = new UpdateCommand({
+            TableName: "quizzes",
+            Key: { quizId: id },
+            UpdateExpression: "SET questions = list_append(if_not_exists(questions, :emptyArray), :newQuestions)",
+            ExpressionAttributeValues: {
+                ":newQuestions": quizQuestions,
+                ":emptyArray": [],
+            },
+        });
+
+        await db.send(updateCommand);
+
+        return sendResponse(200, "Questions added");
+
+    } catch (error) {
+        console.error(error);
+        return sendError(500, "Server error");
+    }
+
 }
 
-exports.handler = middy(addQuestionsHandler).use(authenticate);
+
+exports.handler = middy(addQuestionsHandler).use(authenticate).use(validateQuestions);
